1fd49f9171225a3309548a7c1455952c67983d03
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-12-10 14:26:19 +0900

    Update deep_sleep_component.h

--- esphome/components/deep_sleep/deep_sleep_component.h
+++ esphome/components/deep_sleep/deep_sleep_component.h
@@ -19,17 +19,11 @@ namespace deep_sleep {
 
 #ifdef USE_ESP32
 
-/** The values of this enum define what should be done if deep sleep is set up with a wakeup pin on the ESP32
- * and the scenario occurs that the wakeup pin is already in the wakeup state.
- */
+// Behavior when a wakeup pin is already in the wakeup state at the moment we try to sleep.
 enum WakeupPinMode {
-  WAKEUP_PIN_MODE_IGNORE = 0,  ///< Ignore the fact that we will wake up when going into deep sleep.
-  WAKEUP_PIN_MODE_KEEP_AWAKE,  ///< As long as the wakeup pin is still in the wakeup state, keep awake.
-
-  /** Automatically invert the wakeup level. For example if we were set up to wake up on HIGH, but the pin
-   * is already high when attempting to enter deep sleep, re-configure deep sleep to wake up on LOW level.
-   */
-  WAKEUP_PIN_MODE_INVERT_WAKEUP,
+  WAKEUP_PIN_MODE_IGNORE = 0,    // Ignore and go to deep sleep anyway.
+  WAKEUP_PIN_MODE_KEEP_AWAKE,    // Stay awake while the wakeup pin remains in the wakeup state.
+  WAKEUP_PIN_MODE_INVERT_WAKEUP, // Flip the wakeup level to allow entering deep sleep immediately.
 };
 
 struct Ext1Wakeup {
@@ -38,34 +32,34 @@ struct Ext1Wakeup {
 };
 
 struct WakeupCauseToRunDuration {
-  // Run duration if woken up by timer or any other reason besides those below.
+  // Run duration if woken by timer or any other cause not explicitly listed below.
   uint32_t default_cause;
-  // Run duration if woken up by touch pads.
+  // Run duration if woken by touch pads.
   uint32_t touch_cause;
-  // Run duration if woken up by GPIO pins.
+  // Run duration if woken by GPIO pins.
   uint32_t gpio_cause;
 };
 
-#endif
+#endif  // USE_ESP32
 
 template<typename... Ts> class EnterDeepSleepAction;
-
 template<typename... Ts> class PreventDeepSleepAction;
 
-/** This component allows setting up the node to go into deep sleep mode to conserve battery.
+/**
+ * DeepSleepComponent lets the node enter deep sleep to conserve power.
  *
- * To set this component up, first set *when* the deep sleep should trigger using set_run_cycles
- * and set_run_duration, then set how long the deep sleep should last using set_sleep_duration and optionally
- * on the ESP32 set_wakeup_pin.
+ * Configure:
+ * - set_run_duration / set_run_duration(WakeupCauseToRunDuration) for how long to run before sleeping
+ * - set_sleep_duration for how long to sleep
+ * - on ESP32, optionally set_wakeup_pin and set_wakeup_pin_mode
  */
 class DeepSleepComponent : public Component {
  public:
-  /// Set the duration in ms the component should sleep once it's in deep sleep mode.
+  // Set the duration in ms to sleep once entering deep sleep.
   void set_sleep_duration(uint32_t time_ms);
+
 #if defined(USE_ESP32)
-  /** Set the pin to wake up to on the ESP32 once it's in deep sleep mode.
-   * Use the inverted property to set the wakeup level.
-   */
+  // Set the pin used for GPIO wakeup (level determined by pin invert property).
   void set_wakeup_pin(InternalGPIOPin *pin) { this->wakeup_pin_ = pin; }
 
   void set_wakeup_pin_mode(WakeupPinMode wakeup_pin_mode);
@@ -73,18 +67,18 @@ class DeepSleepComponent : public Component {
 
 #if defined(USE_ESP32)
 #if !defined(USE_ESP32_VARIANT_ESP32C3)
-
+  // Configure EXT1 wakeup (bitmap mask of RTC-capable GPIO and mode).
   void set_ext1_wakeup(Ext1Wakeup ext1_wakeup);
 
+  // Enable/disable touch wakeup support.
   void set_touch_wakeup(bool touch_wakeup);
+#endif  // !USE_ESP32_VARIANT_ESP32C3
 
-#endif
-  // Set the duration in ms for how long the code should run before entering
-  // deep sleep mode, according to the cause the ESP32 has woken.
+  // Cause-aware run duration before entering deep sleep (ESP32 only).
   void set_run_duration(WakeupCauseToRunDuration wakeup_cause_to_run_duration);
-#endif
+#endif  // USE_ESP32
 
-  /// Set a duration in ms for how long the code should run before entering deep sleep mode.
+  // Simple run duration (all platforms).
   void set_run_duration(uint32_t time_ms);
 
   void setup() override;
@@ -93,20 +87,20 @@ class DeepSleepComponent : public Component {
   float get_loop_priority() const override;
   float get_setup_priority() const override;
 
-  /// Helper to enter deep sleep mode
+  // Enter deep sleep manually (immediately or at next loop boundary depending on implementation).
   void begin_sleep(bool manual = false);
 
+  // Temporarily prevent deep sleep; call allow_deep_sleep() to resume.
   void prevent_deep_sleep();
   void allow_deep_sleep();
 
  protected:
-  // Returns nullopt if no run duration is set. Otherwise, returns the run
-  // duration before entering deep sleep.
+  // Returns nullopt if no run duration is set; otherwise, the run duration in ms.
   optional<uint32_t> get_run_duration_() const;
 
   optional<uint64_t> sleep_duration_;
 #ifdef USE_ESP32
-  InternalGPIOPin *wakeup_pin_;
+  InternalGPIOPin *wakeup_pin_{nullptr};
   WakeupPinMode wakeup_pin_mode_{WAKEUP_PIN_MODE_IGNORE};
   optional<Ext1Wakeup> ext1_wakeup_;
   optional<bool> touch_wakeup_;
@@ -121,7 +115,7 @@ extern bool global_has_deep_sleep;  // NOLINT(cppcoreguidelines-avoid-non-const-
 
 template<typename... Ts> class EnterDeepSleepAction : public Action<Ts...> {
  public:
-  EnterDeepSleepAction(DeepSleepComponent *deep_sleep) : deep_sleep_(deep_sleep) {}
+  explicit EnterDeepSleepAction(DeepSleepComponent *deep_sleep) : deep_sleep_(deep_sleep) {}
   TEMPLATABLE_VALUE(uint32_t, sleep_duration);
 
 #ifdef USE_TIME
@@ -138,45 +132,42 @@ template<typename... Ts> class EnterDeepSleepAction : public Action<Ts...> {
     if (this->sleep_duration_.has_value()) {
       this->deep_sleep_->set_sleep_duration(this->sleep_duration_.value(x...));
     }
-#ifdef USE_TIME
 
+#ifdef USE_TIME
+    // If an absolute time-of-day is set, compute ms until that time today/tomorrow and sleep for that duration.
     if (this->hour_.has_value()) {
-      auto time = this->time_->now();
-      const uint32_t timestamp_now = time.timestamp;
+      auto t = this->time_->now();
+      const uint32_t timestamp_now = t.timestamp;
 
       bool after_time = false;
-      if (time.hour > this->hour_) {
+      if (t.hour > *this->hour_) {
         after_time = true;
-      } else {
-        if (time.hour == this->hour_) {
-          if (time.minute > this->minute_) {
+      } else if (t.hour == *this->hour_) {
+        if (t.minute > *this->minute_) {
+          after_time = true;
+        } else if (t.minute == *this->minute_) {
+          if (t.second > *this->second_) {
             after_time = true;
-          } else {
-            if (time.minute == this->minute_) {
-              if (time.second > this->second_) {
-                after_time = true;
-              }
-            }
           }
         }
       }
 
-      time.hour = *this->hour_;
-      time.minute = *this->minute_;
-      time.second = *this->second_;
-      time.recalc_timestamp_utc();
+      t.hour = *this->hour_;
+      t.minute = *this->minute_;
+      t.second = *this->second_;
+      t.recalc_timestamp_utc();
 
-      time_t timestamp = time.timestamp;  // timestamp in local time zone
+      time_t target_local = t.timestamp;  // local time zone
+      if (after_time) {
+        target_local += 60 * 60 * 24;     // move to next day
+      }
 
-      if (after_time)
-        timestamp += 60 * 60 * 24;
-
-      int32_t offset = ESPTime::timezone_offset();
-      timestamp -= offset;  // Change timestamp to utc
-      const uint32_t ms_left = (timestamp - timestamp_now) * 1000;
+      const int32_t offset = ESPTime::timezone_offset();
+      const uint32_t ms_left = static_cast<uint32_t>((target_local - timestamp_now - offset) * 1000);
       this->deep_sleep_->set_sleep_duration(ms_left);
     }
 #endif
+
     this->deep_sleep_->begin_sleep(true);
   }
 
@@ -186,17 +177,18 @@ template<typename... Ts> class EnterDeepSleepAction : public Action<Ts...> {
   optional<uint8_t> hour_;
   optional<uint8_t> minute_;
   optional<uint8_t> second_;
-
-  time::RealTimeClock *time_;
+  time::RealTimeClock *time_{nullptr};
 #endif
 };
 
-template<typename... Ts> class PreventDeepSleepAction : public Action<Ts...>, public Parented<DeepSleepComponent> {
+template<typename... Ts>
+class PreventDeepSleepAction : public Action<Ts...>, public Parented<DeepSleepComponent> {
  public:
   void play(Ts... x) override { this->parent_->prevent_deep_sleep(); }
 };
 
-template<typename... Ts> class AllowDeepSleepAction : public Action<Ts...>, public Parented<DeepSleepComponent> {
+template<typename... Ts>
+class AllowDeepSleepAction : public Action<Ts...>, public Parented<DeepSleepComponent> {
  public:
   void play(Ts... x) override { this->parent_->allow_deep_sleep(); }
 };
